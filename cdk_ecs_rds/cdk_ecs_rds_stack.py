068201f784e651a9491cd865b08bcc27d5891ec9
Barry Dawson <23361254+bazd@users.noreply.github.com>
2021-09-27 16:07:36 +1000

    initial commit new cdk app with ecs only

--- cdk_ecs_rds/cdk_ecs_rds_stack.py
+++ cdk_ecs_rds/cdk_ecs_rds_stack.py
@@ -4,12 +4,29 @@ from aws_cdk import core as cdk
 # the CDK's core module.  The following line also imports it as `core` for use
 # with examples from the CDK Developer's Guide, which are in the process of
 # being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+# from aws_cdk import core
+from aws_cdk import (aws_ec2 as ec2, aws_ecs as ecs, aws_ecs_patterns as
+                     ecs_patterns)
 
 
 class CdkEcsRdsStack(cdk.Stack):
-
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.Construct, construct_id: str,
+                 **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        vpc = ec2.Vpc(self, "MyVpc", max_azs=2)
+
+        cluster = ecs.Cluster(self, "MyCluster", vpc=vpc)
+
+        ecs_patterns.ApplicationLoadBalancedFargateService(
+            self,
+            "MyFargateService",
+            cluster=cluster,  # Required
+            cpu=256,  # Default is 256
+            desired_count=1,  # Default is 1
+            task_image_options=ecs_patterns.
+            ApplicationLoadBalancedTaskImageOptions(
+                image=ecs.ContainerImage.from_registry(
+                    "servian/techchallengeapp")),
+            memory_limit_mib=512,  # Default is 512
+            public_load_balancer=True)  # Default is False
